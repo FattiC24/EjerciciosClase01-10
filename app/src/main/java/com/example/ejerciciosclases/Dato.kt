68c544d03ca9b67c0ea1412b9693db117e5c942e
FattyC24 <62962069+FattyC24@users.noreply.github.com>
2021-10-02 19:38:44 -0600

    Ejercicios

---
+++ app/src/main/java/com/example/ejerciciosclases/Dato.kt
@@ -0,0 +1,19 @@
+package com.example.ejerciciosclases
+
+public class Dato (var numero:Int) {
+    fun lanzar(){
+        var const1 = 1
+        var finalnumber = 0
+
+
+        if(numero >=1 && numero <=6){
+            var random = (1..40).random()
+            finalnumber = random
+            println("El numero es $finalnumber")
+        }
+        else{
+            finalnumber = const1
+            println("Su numero es mayor que 6, entonces $finalnumber")
+        }
+    }
+}
---
+++ app/src/main/java/com/example/ejerciciosclases/Empleado.kt
@@ -0,0 +1,18 @@
+package com.example.ejerciciosclases
+
+class Empleado (var nombre:String, var sueldo:Double, var area:String, var cargo:String,var worktime:Int) {
+    fun sueldo() {
+        if (sueldo < 0) {
+            println("No se permiten sueldos negativos")
+        } else if (area.isEmpty() || cargo.isEmpty()) {
+            println("Rellene todos los campos")
+        }
+        else{
+            var SueldoExtra:Int = 25*(worktime/5)
+            var SueldoTotal = SueldoExtra + sueldo
+
+            println("Nombre $nombre, tiene $worktime a'os de trabajar, su sueldo base es $sueldo y el total por a'os trabjados es $SueldoTotal")
+
+        }
+    }
+}
